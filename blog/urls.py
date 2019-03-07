5fc5811631ef381be745e4c550302faefa9adcc6
avinashgundala <gundala.avinash@gmail.com>
2019-03-07 20:43:34 +0530

    comment updates

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,10 @@ from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView,Pos
 
 urlpatterns = [
     path('',PostListView.as_view(),name='blog-home'),
-    path('post/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
+    path('post/<int:pk>/',views.post_detail,name='post-detail'),
     path('post/new/',PostCreateView.as_view(),name='post-create'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post-update'),
     path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post-delete'),
     path('about/',views.about,name='blog-about'),
-    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
-    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
-    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
-]
+    
+    ]
--- blog/views.py
+++ blog/views.py
@@ -62,28 +62,20 @@ def about(request):
     return render(request,'blog/about.html')
 
 
-#comments section
-def add_comment_to_post(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.save()
-            return redirect('post-detail',pk=post.pk)
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk = pk)
+    comments = Comment.objects.filter(post=post).order_by('-created_date')
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            text = request.POST.get('text')
+            comment_form = Comment.objects.create(post=post,author = request.user,text = text)
+            comment_form.save()
+            return redirect('post-detail',pk = post.pk)
     else:
-        form = CommentForm()
-    return render(request, 'blog/add_comment_to_post.html', {'form': form})
-
-@login_required
-def comment_approve(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    comment.approve()
-    return redirect('post_detail', pk=comment.post.pk)
-
-@login_required
-def comment_remove(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    comment.delete()
-    return redirect('post_detail',pk=comment.post.pk)
+        comment_form = CommentForm()
+    content={'post':post,
+             'comments':comments,
+             'comment_form':comment_form
+              }
+    return render(request,'blog/post_detail.html',content)
